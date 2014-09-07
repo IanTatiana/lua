021b9bfd91ec69e76472a8e3976aad6cd9b5203e
Tatiana <information_element@mail.ru>
2014-09-07 19:48:07 +1100

    Add tokens for SWITCH-statement

--- src/llex.h
+++ src/llex.h
@@ -25,13 +25,15 @@ enum RESERVED {
   TK_DO, TK_ELSE, TK_ELSEIF, TK_END, TK_FALSE, TK_FOR, TK_FUNCTION,
   TK_GOTO, TK_IF, TK_IN, TK_LOCAL, TK_NIL, TK_NOT, TK_OR, TK_REPEAT,
   TK_RETURN, TK_THEN, TK_TRUE, TK_UNTIL, TK_WHILE,
+  /* switch statement tokens*/
+  TK_SWITCH, TK_CASE, TK_DEFAULT, TK_BEGIN,
   /* other terminal symbols */
   TK_CONCAT, TK_DOTS, TK_EQ, TK_GE, TK_LE, TK_NE, TK_DBCOLON, TK_EOS,
   TK_NUMBER, TK_NAME, TK_STRING
 };
 
 /* number of reserved words */
-#define NUM_RESERVED	(cast(int, TK_WHILE-FIRST_RESERVED+1))
+#define NUM_RESERVED	(cast(int, TK_BEGIN-FIRST_RESERVED+1))
 
 
 typedef union {
